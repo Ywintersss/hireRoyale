d08c113444bf6cc2ba580ed45bad8cb6a8a80544
Cindy Pua <cindyqi0511@gmail.com>
2025-08-30 15:43:52 +0800

    Update socket.ts

--- talentacquisition/frontend/src/lib/socket.ts
+++ talentacquisition/frontend/src/lib/socket.ts
@@ -1,9 +1,4 @@
-import { io } from "socket.io-client";
-
-
-export const socket = io("http://localhost:8000", {
-    //options
-});'use client'
+'use client'
 import { io, Socket } from 'socket.io-client'
 
 let socket: Socket | null = null
